f5dd0dc200a2b9b394ad8ee976ed72634e0232f6
Jack Firth <jackhfirth@gmail.com>
2024-08-11 22:35:39 -0700

    Run `raco pkg new`

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "guard")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/guard.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(jackfirth))
+(define license '(Apache-2.0 OR MIT))
---
+++ scribblings/guard.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[guard
+                    racket/base]]
+
+@title{guard}
+@author{jackfirth}
+
+@defmodule[guard]
+
+Package Description Here
